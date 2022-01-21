ae7006f78a2d8ebc05c12ed77f60dc47528fc890
oseg1 <oseg1@online.fr>
2022-01-21 17:11:47 +0100

    fix(views): msoffice url

--- views/default/elgg_file_viewer/apps/msoffice.php
+++ views/default/elgg_file_viewer/apps/msoffice.php
@@ -7,7 +7,7 @@ if (!$entity instanceof ElggFile) {
 }
 
 $url = urlencode(elgg_file_viewer_get_public_url($entity));
-$iframe_url = "http://view.officeapps.live.com/op/view.aspx?src=$url";
+$iframe_url = "https://view.officeapps.live.com/op/view.aspx?src=$url";
 
 $attr = elgg_format_attributes(array(
 	'src' => $iframe_url,
